affe10504c3323401888946d1afde6fb235ff2f0
DWaidP <kevinsalinasvd@gmail.com>
2022-12-31 00:12:28 -0500

    Initial commit

---
+++ src/TAREA1.java
@@ -0,0 +1,16 @@
+public class TAREA1 {
+
+    public static void main(String[] args){
+
+        resta(20, 5, 3);
+    }
+
+    public static int resta(int num1, int num2, int num3){
+
+        int resultado = num1 - num2 - num3;
+        System.out.println(resultado);
+
+        return resultado;
+
+    }
+}
